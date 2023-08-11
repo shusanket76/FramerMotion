50d5b4dfb8380e62edb196d103e1a69bf8c3d107
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-11 10:44:58 -0400

    text-animation

--- framermotion/src/App.jsx
+++ framermotion/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import { motion } from "framer-motion";
 const App = () => {
   return (
-    <div className='text-4xl font-light'>App</div>
-  )
-}
+    <motion.h1
+      className="text-center text-5xl"
+      // initial={{ y: -102 }}
+      animate={{ y: [0, 300, 0] }}
+      transition={{ repeat: Infinity, duration: "2" }}
+    >
+      MY NAME IS SHUSANKET BASYAL AND I LIVE IN NEW YORK
+    </motion.h1>
+  );
+};
 
-export default App
+export default App;
