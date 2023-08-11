25c2690b24c25fdfed0f3913fe2c222b86ec0fcb
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-11 11:52:13 -0400

    puzzle

--- framermotion/src/App1.jsx
+++ framermotion/src/App1.jsx
@@ -5,47 +5,47 @@ const App1 = () => {
   return (
     <div className="grid gap-1 p-20 grid-cols-3">
       <motion.img
-        src="./img/image1x1.jpg"
+        src="src/img/image1x1.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
       <motion.img
-        src="./img/image1x2.jpg"
+        src="src/img/image1x2.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
       <motion.img
-        src="./img/image1x3.jpg"
+        src="src/img/image1x3.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
       <motion.img
-        src="./img/image2x1.jpg"
+        src="src/img/image2x1.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
       <motion.img
-        src="./img/image2x2.jpg"
+        src="src/img/image2x2.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
       <motion.img
-        src="./img/image2x3.jpg"
+        src="src/img/image2x3.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
       <motion.img
-        src="./img/image3x1.jpg"
+        src="src/img/image3x1.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
       <motion.img
-        src="./img/image3x2.jpg"
+        src="src/img/image3x2.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
       <motion.img
-        src="./img/image3x3.jpg"
+        src="src/img/image3x3.jpg"
         className="h-60 w-60"
         drag
       ></motion.img>
